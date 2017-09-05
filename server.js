907a9e4d912ca83d8decf7e6a640a36a6bc0900a
OoI3enZaoO <Teerapath-Ben@hotmail.com>
2017-09-05 11:45:38 +0700

    socket.io

--- server.js
+++ server.js
@@ -10,71 +10,75 @@ var port = process.env.PORT || 5000
 var path = require('path');
 var bodyParser = require("body-parser");
 
+
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 // app.use(bodyParser.json());
 app.use(cors())
-app.use(Express.static(path.join(__dirname, '/public')));
-app.get('/', function (req, res) {
- res.redirect('index.html')
-})
+// app.use(Express.static(path.join(__dirname, '/public')));
 
-app.get('/visualizar', function (req, res) {
- res.redirect('visualizar.html')
+app.get('/', function (req, res) {
+  res.send(`<h1>555 Hello Vue.js 2 socket. io </h1> <br> <li> yarn add socket </li> <li> function socket.io  </li>`)
+//   res.redirect('index.html')
 })
 
 io.on('connection', function (socket) {
-  console.log('a user connected ', socket.id)
-  // user ที่เปิดหรืออก browser
-  socket.on('disconnect', function () {
-    console.log('user disconnected ', socket.id)
-  })
-  socket.on('subscribe', function(room) {
-    console.log('joining room', room);
-    socket.join(room);
-  });
-  socket.on('leaveRoom',function(room){
-    console.log('leave Room', room);
-    socket.leave(room)
-  });
-  socket.on('private_message',function(data){
-    console.log("send from room: " + data.room);
-    io.to(data.room).emit('conversation_private', data);
-    io.emit('admin', data)
-  });
-  socket.on('toUser',function(data){
-      data.type = "admin"
-      io.to(data.room).emit('fromAdmin', data)
-  })
-  socket.on('addNewCourse' ,function(data){
-    console.log('addNewCourse: ' + data)
-      io.emit('newCardData',data)
-  })
-  socket.on('removeCourse' ,function(data){
-    console.log('removeCourse: ' + data)
-      io.emit('removeCourse',data)
-  })
-	socket.on('stream',function(image) {
-		socket.broadcast.emit('stream',image)
-	});
-
-  socket.on('joinroom', function(room) {
-    console.log('joining room', room);
-    socket.join(room);
-  });
-
-  socket.on('sendvideo',function(data){
-      io.to(data.room).emit('sendtoyou', data.data)
-  });
-
-  socket.on('leaveRoom',function(room){
-    console.log('leave Room', room);
-    socket.leave(room)
-  });
-  
-
+    console.log('user connected: ' + socket.id);
+    socket.on('subscribe', function (room) {
+      socket.join(room)
+      console.log('room: ' + room)
+    })
+    socket.on('private_message' ,function (data) {
+      io.to(data.room).emit('conversation_private', data)
+    })
 })
+
+// io.on('connection', function (socket) {
+//   socket.on('stream', function (image) {
+//     socket.broadcast.emit('stream', image)
+//   })
+// })
+// บ่งบอกสถานะคนที่เข้ามาใช้งาน โดยจะมีหมายเลข socket.id เเตกต่างกันครับ
+// io.on('connection', function (socket) {
+//   console.log('a user connected ', socket.id)
+//   // user ที่เปิดหรืออก browser
+//   socket.on('disconnect', function () {
+//     console.log('user disconnected ', socket.id)
+//   })
+//   socket.on('subscribe', function(room) {
+//     console.log('joining room', room);
+//     socket.join(room);
+//   });
+//   socket.on('leaveRoom',function(room){
+//     console.log('leave Room', room);
+//     socket.leave(room)
+//   });
+//   socket.on('private_message',function(data){
+//     console.log("send from room: " + data.room);
+//     io.to(data.room).emit('conversation_private', data);
+//     io.emit('admin', data)
+//   });
+//   socket.on('toUser',function(data){
+//       data.type = "admin"
+//       io.to(data.room).emit('fromAdmin', data)
+//   })
+//   socket.on('addNewCourse' ,function(data){
+//     console.log('addNewCourse: ' + data)
+//       io.emit('newCardData',data)
+//   })
+//   socket.on('removeCourse' ,function(data){
+//     console.log('removeCourse: ' + data)
+//       io.emit('removeCourse',data)
+//   })
+//
+//   // รับเฉพาะ Event ข้อความ จาก client
+//   socket.on('chat_message', function (msg) {
+//     console.log('socket by : ', socket.id, ' message: ' + msg)
+//     // ส่งข้อมูลกลับไปหาผู้ส่งมา
+//     io.emit('chat_message', msg)
+//   })
+// })
 
 http.listen(port, function () {
   log.info('Run Port // localhost:', port)
